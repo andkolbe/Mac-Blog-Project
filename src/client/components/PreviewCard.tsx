68d69bde7f6ea3e818d556f5a4c65869072b619a
Andrew Kolbe <andrewkolbe@Erins-MacBook-Pro.local>
2021-01-19 17:17:55 -0500

    Changed Size of Cards

--- src/client/components/PreviewCard.tsx
+++ src/client/components/PreviewCard.tsx
@@ -6,13 +6,13 @@ import { Link } from 'react-router-dom';
 const PreviewCard: React.FC<PreviewCardProps> = ({ blog }) => { // FC stands for function component. PreviewCard is a function component
 
     return (
-        <div className="col-md-4">
+        <div className="col-md-10">
             <article className="card my-2 shadow">
                 <div className="card-body">
                     <h4 className="card-title">{blog.title}</h4>
                     <p className="card-text">{blog.content.substring(0, 125)}</p> 
                     <div className="d-flex justify-content-between">
-                        <small className="card-text text-secondary">{moment(blog.created_at).format('dddd')}</small>
+                        <small className="card-text text-secondary">{moment(blog.created_at).format('ll')}</small>
                         <Link to={`/details/${blog.id}`}>Read more!</Link>
                     </div>
 
